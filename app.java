be6bd78e43d4924d60f9c8446c65dc9006ae216b
Abdullahi <muxsinxareed6@gmail.com>
2023-10-02 22:27:52 +0200

    first commit

---
+++ app.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+
+public class app {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // information om vart personen står
+        String[] rum = {
+                "Du står framför ett Hus, tryck på ENTER för gå in i huset",
+                "Du står i entren",
+                "Tryck Enter för att gå in i köket",
+                "Du är inne köket",
+                "Tryck Enter för att få äta ett frukt",
+                "tryck enter för att drycka vatten",
+                "tryck enter för att gå ut från köket",
+                "tryck enter för att kolla på rum 1",
+                "Redo att gå ut, tryck enter för att gå ut ur rumm1",
+                "Du är ute, tryck enter för att gå in rum2",
+                "Du är inne i rum2, tryck enter för att lägga dig",
+                "Du ligger på sängen",
+                "Tryck enter för att gå upp från sängen",
+                "Tryck Enter för att gå ut från rummet",
+                "Du är ute, tryck enter för att gå till rum3",
+                "Du är framför rum3 du behöver nyckeln för att gå in",
+                "Tryck Enter för gå till källan och hämta nyckel",
+                "Tryck Enter för att gå till trappan",
+                "Du står framför trappan",
+                "Tryck Enter för att gå ner",
+                "Du står framför en dörr",
+                "Tryck Enter för att gå in källaren",
+                "Game Over, Du är i mitt fälla"
+        };
+        int steg = 0;
+
+        // print information in the rum[] array
+        System.out.println(rum[0]);
+        sc.nextLine();
+        steg+=1;
+        System.out.println(rum[1]);
+        System.out.println(rum[2]);
+        sc.nextLine();
+
+        System.out.println(rum[3]);
+        System.out.println(rum[4]);
+        sc.nextLine();
+
+        System.out.println(rum[5]);
+        sc.nextLine();
+
+        System.out.println(rum[6]);
+        sc.nextLine();
+
+        System.out.println(rum[7]);
+        sc.nextLine();
+        System.out.println(rum[8]);
+        sc.nextLine();
+
+        System.out.println(rum[9]);
+        sc.nextLine();
+
+        System.out.println(rum[10]);
+        sc.nextLine();
+
+        System.out.println(rum[11]);
+        sc.nextLine();
+
+        System.out.println(rum[12]);
+        sc.nextLine();
+
+        System.out.println(rum[13]);
+        sc.nextLine();
+
+        System.out.println(rum[14]);
+        sc.nextLine();
+        System.out.println(rum[15]);
+        sc.nextLine();
+
+        System.out.println(rum[16]);
+        sc.nextLine();
+
+        System.out.println(rum[17]);
+        sc.nextLine();
+        System.out.println(rum[18]);
+        System.out.println(rum[19]);
+        sc.nextLine();
+
+
+        System.out.println(rum[20]);
+        System.out.println(rum[21]);
+        sc.nextLine();
+
+        System.out.println(rum[22]);
+
+
+
+
+
+
+
+
+
+
+        
+
+    }
+}
